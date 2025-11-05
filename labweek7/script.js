a4896fbc72bb53a3deb3ddb366989041370c5779
hwzieba <48774964+hwzieba@users.noreply.github.com>
2025-11-05 23:45:31 +0000

    Deleting lab7

--- labweek7/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    event.preventDefault();
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    document.getElementById("myForm").style.display="none";
-    document.getElementById("thanks").innerHTML = "Thank you for subscribing, <b>"+name+"</b>!<br><br>We will be in touch via <b>"+email+"</b> shortly!";
-    document.getElementById("thanks").style.display="block";
-}
